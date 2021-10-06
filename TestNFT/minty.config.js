f5a50fb5d1f16b7dd8eb709e6dde6f202c4a2b92
10thfloor <remember.mackenzie@gmail.com>
2021-10-05 17:30:38 -0700

    Remove generated project.

--- TestNFT/minty.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  pinningService: {
-    name: process.env.PINNING_SERVICE_NAME,
-    endpoint: process.env.PINNING_SERVICE_ENDPOINT,
-    key: process.env.PINNING_SERVICE_KEY
-  },
-}
--- TestNFT/web/hooks/use-current-user.js
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect } from "react";
-import * as fcl from "@onflow/fcl";
-
-export default function useCurrentUser() {
-  const [currentUser, setCurrentUser] = useState(null);
-  useEffect(() => fcl.currentUser().subscribe(setCurrentUser), []);
-  return currentUser;
-}
