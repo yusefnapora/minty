c7b0a33c6c991c72f69e6da0a397c4e2f569df7b
Lonis Hamaili <lonis@Loniss-MBP.lan>
2021-11-05 16:25:29 +0000

    go-ipfs not needed to run daemon

--- start-local-environment.sh
+++ start-local-environment.sh
@@ -14,6 +14,6 @@ fi
 
 echo "Running IPFS and development blockchain"
 run_eth_cmd="npx hardhat node"
-run_ipfs_cmd="npx go-ipfs daemon"
+run_ipfs_cmd="ipfs daemon"
 
 npx concurrently -n eth,ipfs -c yellow,blue "$run_eth_cmd" "$run_ipfs_cmd"
