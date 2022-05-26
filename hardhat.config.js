5f5332bee6b54e9ed339bb613867e2fd005099f9
ceobitch <ceo@madebyayman.com>
2022-05-26 17:59:44 -0500

    Ether Scan Key ✅✅✅
    I added the etherscan key to hardhat config file. Why? This will give us the ability to verify our nft contracts via command line 'npx hardhat verify {CONTRACT_ADDRESS}"

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,8 @@
  */
 
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();
 const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
@@ -21,8 +23,8 @@ module.exports = {
             url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
             accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
           },
-        //   etherscan: {
-        //     apiKey: ETHERSCAN_API_KEY,
-        //   },
-    }
-};
+        },
+          etherscan: {
+            apiKey: ETHERSCAN_API_KEY,
+          },
+}
