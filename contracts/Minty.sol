2a7ab269cc081ec250045db1c3f70319a11d1d09
ceobitch <ceo@madebyayman.com>
2022-05-26 16:58:08 -0500

    Enhanced Contract 🥳
    I upgraded the Minty contract to accept payments, and also updated the code so it can be compatible with solidity version 0.8.1.

--- contracts/Minty.sol
+++ contracts/Minty.sol
@@ -1,21 +1,24 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Minty is ERC721 {
+contract Minty is ERC721, PullPayment, Ownable  {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-
+    string public baseTokenURI;
+    mapping (uint256 => string) private _tokenURIs;
+    
     constructor(string memory tokenName, string memory symbol) ERC721(tokenName, symbol) {
-        _setBaseURI("ipfs://");
+        baseTokenURI = "ipfs://";
     }
 
     function mintToken(address owner, string memory metadataURI)
-    public
-    returns (uint256)
+    public payable returns (uint256)
     {
         _tokenIds.increment();
 
@@ -25,4 +28,36 @@ contract Minty is ERC721 {
 
         return id;
     }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+            require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+            _tokenURIs[tokenId] = _tokenURI;
+        }
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+            require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+            string memory _tokenURI = _tokenURIs[tokenId];
+            string memory base = _baseURI();
+            
+            // If there is no base URI, return the token URI.
+            if (bytes(base).length == 0) {
+                return _tokenURI;
+            }
+            // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+            if (bytes(_tokenURI).length > 0) {
+                return string(abi.encodePacked(base, _tokenURI));
+            }
+            // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
+            return string(abi.encodePacked(base, tokenId));
+        }
+  /// @dev Returns an URI for a given token ID
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseTokenURI;
+  }
+
+  /// @dev Sets the base token URI prefix.
+  function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
+    baseTokenURI = _baseTokenURI;
+  }
+
 }
